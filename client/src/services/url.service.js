ba97acd1c3c501adbfab1714ae9623e290947c98
Naveenchinthakindi <nchinthakindi143@gmail.com>
2025-07-28 10:40:34 +0530

    added env

--- client/src/services/url.service.js
+++ client/src/services/url.service.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 const API_URL = `${import.meta.env.VITE_APP_API_URL}/api`;
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_DEV_URL
+    : import.meta.env.VITE_PROD_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
